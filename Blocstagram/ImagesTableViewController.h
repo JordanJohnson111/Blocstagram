f7fd8e2b0eec5e0dec44120771eed568217d1c1f
jordanjohnson111 <jordanjohnson111@gmail.com>
2016-08-25 12:24:59 -0500

    Checkpoint 27 completed

--- Blocstagram/ImagesTableViewController.h
+++ Blocstagram/ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Jordan Johnson on 8/25/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ImagesTableViewController : UITableViewController
 
 @end
-
